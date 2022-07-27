3cdffab4143409b59cb4f05662d4b288a6864afb
DzKalinin <dmitrykalinin94@gmail.com>
2022-07-27 22:21:59 +0400

    update time zone to UTC

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 ENV["RACK_ENV"] ||= 'development'
 require './dependencies'
 
-Time.zone = ActiveSupport::TimeZone['Eastern Time (US & Canada)']
-Time.zone_default = ActiveSupport::TimeZone['Eastern Time (US & Canada)']
+Time.zone = ActiveSupport::TimeZone['UTC']
+Time.zone_default = ActiveSupport::TimeZone['UTC']
